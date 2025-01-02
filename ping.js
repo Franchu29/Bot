b9431cf006cd59a58fa97eecbe27a3b6013175df
Franchu <102395117+Franchu29@users.noreply.github.com>
2025-01-02 00:13:26 -0300

    Add files via upload

---
+++ ping.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+
+// URL del servidor
+const pingUrl = 'https://translucent-uttermost-paradox.glitch.me/status';
+
+function autoPing() {
+  axios
+    .get(pingUrl)
+    .then((response) => {
+      console.log(`Auto-ping exitoso: ${response.status} URL: ${pingUrl}`);
+    })
+    .catch((error) => {
+      console.error('Error en el auto-ping:', error.message);
+    });
+}
+
+// Exportar la función para ser usada en el archivo principal
+module.exports = () => {
+  // Ejecutar auto-ping cada 5 minutos
+  setInterval(autoPing, 60000);
+};
